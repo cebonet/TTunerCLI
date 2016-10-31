4af9ba00b946f527ab60822db6a00766c8b5930e
Cebrail Erdogan <cebonet@msn.com>
2016-10-31 21:04:27 +0100

    Testing of autocorrelations added

--- Makefile
+++ Makefile
@@ -3,20 +3,18 @@ OBJ_FILES := $(addprefix obj/,$(notdir $(CPP_FILES:.cpp=.o)))
 LD_FLAGS = -lrt -lm -lasound -ljack -pthread -lsndfile 
 LIB_FLAGS = lib/libportaudio.a
 
-CPP_TEST_FILES := $(wildcard tests/*.cpp)
+CPP_TEST_FILES := $(filter-out src/TerminalTuner.cpp src/recordaudio.cpp, $(wildcard */*.cpp))
 OBJ_TEST_FILES := $(addprefix obj/,$(notdir $(CPP_TEST_FILES:.cpp=.o)))
 
 all : TerminalTuner run
 
-testProject: TerminalTuner test runTest
-
-test: $(OBJ_TEST_FILES) 
-	g++ $(LD_FLAGS) -o $@ $^ $(LIB_FLAGS)
-
+testProject: test runTest
 
 TerminalTuner: $(OBJ_FILES)
 	g++ $(LD_FLAGS) -o $@ $^ $(LIB_FLAGS)
 
+test: $(OBJ_TEST_FILES) 
+	g++ $(LD_FLAGS) -o $@ $^ 
 
 obj/%.o: src/%.cpp
 	g++ $(CC_FLAGS) -c -o $@ $<
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,13 +1,97 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "portaudio.h"
+#include "../src/autocorrelation.h"
+#include "../src/wavReader.h"
+#include "../src/peakpicking.h"
+#include "../src/peakpicking.h"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+
+
+#define SAMPLE_RATE 44100
+#define WINDOW_SIZE 1024
+#define WIN_HAMMING 1
+#define WIN_HANN 2
+#define WIN_SINE 3
+
+double truncate(double v, int p){
+    double f = pow(10,p);
+    return round(v*f)/f;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 2 );
-    REQUIRE( Factorial(2) == 3 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+SCENARIO("Pitch from pure sinus signal 329.628:", "[pitch autocorrelation 329.628]")
+{
+    GIVEN("Buffers and sinus signal")
+    {
+        double period, frequency;
+
+        // Buffers
+        double* buffer_pcm = (double*) malloc(sizeof(double)*WINDOW_SIZE);
+        double* buffer_window = (double*) malloc(sizeof(double)*WINDOW_SIZE);
+        double* buffer_acf = (double*) malloc(sizeof(double)*WINDOW_SIZE);
+        double* buffer_acf2 = (double*) malloc(sizeof(double)*WINDOW_SIZE);
+        double* buffer_nacf2 = (double*) malloc(sizeof(double)*WINDOW_SIZE);
+        double* buffer_acf2_win = (double*) malloc(sizeof(double)*WINDOW_SIZE);
+        double* buffer_snac = (double*) malloc(sizeof(double)*WINDOW_SIZE);
+        double* buffer_wsnac = (double*) malloc(sizeof(double)*WINDOW_SIZE);
+      
+        // Instantiate
+        wavReader r = wavReader("soundSamples/329.628.wav", WINDOW_SIZE);
+        peakpicking pick = peakpicking(WINDOW_SIZE);
+        autocorrelation acf = autocorrelation(WINDOW_SIZE);
+
+        // getAudio 
+        int sr = r.getWavBuffer(buffer_pcm); 
+       
+        // Apply functions
+        acf.window(WIN_HANN, buffer_window);
+        acf.autocorrelation_acf(buffer_pcm, buffer_acf);
+        acf.autocorrelation_acf2(buffer_pcm, buffer_acf2);
+        acf.autocorrelation_acf2(buffer_pcm, buffer_acf2_win, buffer_window);
+        acf.autocorrelation_nacf2(buffer_pcm, buffer_nacf2);
+        acf.autocorrelation_snac(buffer_pcm, buffer_snac);
+        acf.autocorrelation_snac(buffer_pcm, buffer_wsnac, buffer_window);
+
+        WHEN("We use ACF")
+        {
+            period = pick.getPeriod(buffer_acf);
+                REQUIRE(truncate(sr/period,3) == 329.628 );
+        }
+
+        WHEN("We use ACF type 2")
+        {
+            period = pick.getPeriod(buffer_acf2);
+                REQUIRE(truncate(sr/period,3) == 329.628 );
+        }
+
+        WHEN("We use normalized - ACF type 2")
+        {
+            period = pick.getPeriod(buffer_nacf2);
+                REQUIRE(truncate(sr/period,3) == 329.628 );
+        }
+        
+        WHEN("We use windowed ACF type 2")
+        {
+            period = pick.getPeriod(buffer_acf2_win);
+                
+            REQUIRE(truncate(sr/period,3) == 329.628 );
+        }
+        
+        WHEN("We use SNAC")
+        {
+            period = pick.getPeriod(buffer_snac);
+                REQUIRE(truncate(sr/period,3) == 329.628 );
+        }
+        
+        WHEN("We use windowed SNAC")
+        {
+            period = pick.getPeriod(buffer_wsnac);
+                REQUIRE(truncate(sr/period,3) == 329.628 );
+        }
+    }
 }
+
+
+
+
+
